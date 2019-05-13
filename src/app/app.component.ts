aaf00685342ab64deba72cd2bbbb7a98a80a8d49
ShahinAlKabirMitul <mitul.cse@gmail.com>
2019-05-13 22:41:21 +0600

    Login scratch

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsService } from './@core/utils/analytics.service';
+
+@Component({
+  selector: 'ngx-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  isLogIn = false;
+  constructor(private analytics: AnalyticsService) {}
+
+  ngOnInit() {
+    this.analytics.trackPageViews();
+  }
+}
---
+++ src/app/loginnew/loginnew.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'loginnew',
+  templateUrl: './loginnew.component.html',
+  styleUrls: ['./loginnew.component.scss']
+})
+export class LoginnewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
